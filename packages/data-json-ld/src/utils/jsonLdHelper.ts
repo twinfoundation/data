6fd8d321fcd9b2cd18588b834b9d68ee04859d75
Jose M. Cantera <jose.cantera@iota.org>
2025-05-27 17:27:37 +0200

    feat: ensure backwards compatibility

--- packages/data-json-ld/src/utils/jsonLdHelper.ts
+++ packages/data-json-ld/src/utils/jsonLdHelper.ts
@@ -15,32 +15,46 @@ export class JsonLdHelper {
 	 * @param document The JSON-LD document to validate.
 	 * @param validationFailures The list of validation failures to add to.
 	 * @param validationMode The validation mode to use, defaults to either.
+	 * @param expand Whether the JSON-Ld document has to be expanded.
 	 * @returns True if the document was valid.
 	 */
 	public static async validate<T extends IJsonLdDocument = IJsonLdDocument>(
 		document: T,
 		validationFailures: IValidationFailure[],
-		validationMode?: ValidationMode
+		validationMode?: ValidationMode,
+		expand?: boolean
 	): Promise<boolean> {
 		if (Is.array<IJsonLdNodeObject>(document)) {
 			// If the document is an array of nodes, validate each node
 			for (const node of document) {
-				await JsonLdHelper.validate(node, validationFailures, validationMode);
+				await JsonLdHelper.validate(node, validationFailures, validationMode, expand);
 			}
 		} else if (Is.array<IJsonLdNodeObject>(document["@graph"])) {
 			// If the graph is an array of nodes, validate each node
 			for (const node of document["@graph"]) {
-				await JsonLdHelper.validate(node, validationFailures, validationMode);
+				await JsonLdHelper.validate(node, validationFailures, validationMode, expand);
 			}
 		} else if (Is.object<IJsonLdNodeObject>(document)) {
-			// If the graph is a single node, then use the validate the node schema
-			const expandedDoc = await JsonLdProcessor.expand(document);
-			const expandedDataType = ArrayHelper.fromObjectOrArray(expandedDoc[0]["@type"]);
-			if (Is.arrayValue(expandedDataType)) {
-				await DataTypeHelper.validateWithExpandedType(
+			const dataType = ArrayHelper.fromObjectOrArray(document["@type"])[0];
+			// If JSON-LD has to be expanded the data type will be the expanded one
+			if (expand) {
+				// If the graph is a single node, then use the validate the node schema
+				const expandedDoc = await JsonLdProcessor.expand(document);
+				const expandedDataType = ArrayHelper.fromObjectOrArray(expandedDoc[0]["@type"]);
+				if (Is.arrayValue(expandedDataType)) {
+					await DataTypeHelper.validateWithExpandedType(
+						"document",
+						dataType,
+						expandedDataType[0],
+						document,
+						validationFailures,
+						validationMode
+					);
+				}
+			} else if (Is.stringValue(dataType)) {
+				await DataTypeHelper.validate(
 					"document",
-					ArrayHelper.fromObjectOrArray(document["@type"])[0],
-					expandedDataType[0],
+					dataType,
 					document,
 					validationFailures,
 					validationMode
